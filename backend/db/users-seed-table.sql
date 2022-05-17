296cc48230d3957618a068435a8d1c11fcb20716
Brayan Lopez <brxyxn.corp@live.com>
2022-05-17 00:11:19 -0600

    created sql files

---
+++ backend/db/users-seed-table.sql
@@ -0,0 +1,11 @@
+INSERT INTO users(name, email, password) VALUES ('My Name 1', 'user@example.com', 'mypassword');
+INSERT INTO users(name, email, password) VALUES ('My Name 2', 'user@example.com', 'mypassword');
+INSERT INTO users(name, email, password) VALUES ('My Name 3', 'user@example.com', 'mypassword');
+INSERT INTO users(name, email, password) VALUES ('My Name 4', 'user@example.com', 'mypassword');
+INSERT INTO users(name, email, password) VALUES ('My Name 5', 'user@example.com', 'mypassword');
+INSERT INTO users(name, email, password) VALUES ('My Name 6', 'user@example.com', 'mypassword');
+INSERT INTO users(name, email, password) VALUES ('My Name 7', 'user@example.com', 'mypassword');
+INSERT INTO users(name, email, password) VALUES ('My Name 8', 'user@example.com', 'mypassword');
+INSERT INTO users(name, email, password) VALUES ('My Name 9', 'user@example.com', 'mypassword');
+INSERT INTO users(name, email, password) VALUES ('My Name 10', 'suer@example.com', 'mypassword');
+INSERT INTO users(name, email, password) VALUES ('My Name 11', 'suer@example.com', 'mypassword');
