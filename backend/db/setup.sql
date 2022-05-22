925d0c3fa845f27136c2a4457446a3be7c6b4816
Brayan Lopez <brxyxn.corp@live.com>
2022-05-21 21:31:17 -0600

    updated grant on db and added grant on table users

--- backend/db/setup.sql
+++ backend/db/setup.sql
@@ -1,4 +1,4 @@
 -- CREATE DATABASE nclouds_db;
 /**/;
 CREATE USER backenduser WITH PASSWORD 'backendpassword';
-GRANT ALL ON DATABASE nclouds_db TO backenduser;
+GRANT ALL PRIVILEGES ON DATABASE nclouds_db TO backenduser;
