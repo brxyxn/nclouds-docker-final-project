910b925c6a46ffd04c37bc1858ba5fed0a25df9c
Brayan Lopez <brxyxn.corp@live.com>
2022-05-22 00:11:35 -0600

    updated logger and responders

--- backend/utils/functions.go
+++ backend/utils/functions.go
@@ -15,34 +15,34 @@ This functions are suitable for logging into the console using some
 prefix to identify better the nature of the errors and logs.
 */
 type logging struct {
-	P *log.Logger
+	p *log.Logger
 }
 
 var Log *logging
 var l logging
 
 func InitLogs(prefix string) *log.Logger {
-	l.P = log.New(os.Stdout, prefix, log.LstdFlags)
+	l.p = log.New(os.Stdout, prefix, log.LstdFlags)
 	l.Info("Initializing logs...")
-	return l.P
+	return l.p
 }
 
 func (lg *logging) Info(x ...interface{}) {
 	s := "[INFO] "
 	args := append([]interface{}{s}, x...)
-	l.P.Println(args...)
+	l.p.Println(args...)
 }
 
 func (lg *logging) Error(x ...interface{}) {
 	s := "[ERROR]"
 	args := append([]interface{}{s}, x...)
-	l.P.Println(args...)
+	l.p.Println(args...)
 }
 
 func (lg *logging) Debug(x ...interface{}) {
 	s := "[DEBUG]"
 	args := append([]interface{}{s}, x...)
-	l.P.Println(args...)
+	l.p.Println(args...)
 }
 
 /*
@@ -57,20 +57,25 @@ func DotEnvGet(key string) string {
 	return os.Getenv(key)
 }
 
+
+/* HTTP Responders */
+type response struct{}
+var Respond response
+
 /*
 This function responds the information successfully as JSON.
 */
-func RespondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
-	response, _ := json.Marshal(payload)
+func (r response) JSON(w http.ResponseWriter, code int, payload interface{}) {
+	res, _ := json.Marshal(payload)
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(code)
-	w.Write(response)
+	w.Write(res)
 }
 
 /*
 This function responds an error message to the frontend as JSON.
 */
-func RespondWithError(w http.ResponseWriter, code int, message string) {
-	RespondWithJSON(w, code, map[string]string{"error": message})
+func (r response) Error(w http.ResponseWriter, code int, message string) {
+	r.JSON(w, code, map[string]string{"error": message})
 }
